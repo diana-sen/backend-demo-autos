1f97b0b4a31288acd59df3bb833d817ef3d9c08d
Diana Sen <dianasen13@gmail.com>
2023-02-03 01:56:26 -0600

    feat: connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,8 @@ app.listen(port, () =>console.log(`server started on port ${port}`))
 
 app.use('/api/autos',require('./routes/autosRoutes'))
 
+
+//connect to db
+const colors = require('colors')
+const connectDB = require('./config/db')
+connectDB()
